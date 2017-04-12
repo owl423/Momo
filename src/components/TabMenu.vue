e16f449fe3065b54f3f8130921fa2fd350990ff0
hyunji-bang <groovehyunji@gmail.com>
2017-04-12 14:54:07 +0900

    사용자 메뉴 수정

---
+++ src/components/TabMenu.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="tabmenu-wrap">
+        <div class="tabmenu">
+            <h2>내 지도 리스트</h2>
+            <div class="tabmenu__navi">
+                <ul>
+                    <li class="tabmenu__navi__button tabmenu__navi--active"><a href="">지도</a></li>
+                    <li class="tabmenu__navi__button"><a href="">이미지</a></li>
+                </ul>
+            </div>
+            <div class="tabmenu__view tabmenu__view--map">
+                <ul>
+                    <li>나의 최애 커피</li>
+                    <li>나의 최애 커피</li>
+                    <li>나의 최애 커피</li>
+                    <li>나의 최애 커피</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'tab-menu',
+  data (){
+      return {
+
+      }
+  },
+  methods: {
+
+  }
+}
+</script>
--- src/components/UserMenu.vue
+++ src/components/UserMenu.vue
@@ -5,20 +5,29 @@
                 <h1>나의 장소</h1>
                 <button class="usermenu__button" @click="$emit('menuClose')"> < </button>
             </div>
+            <div class="usermenu__view">
+                <Tabmenu></Tabmenu>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+
+import Tabmenu from './TabMenu.vue';
+
 export default {
-  name: 'user-menu',
-  data (){
-      return {
+    name: 'user-menu',
+    components: {
+        Tabmenu
+    },
+    data (){
+        return {
 
-      }
-  },
-  methods: {
+        }
+    },
+    methods: {
 
-  }
-}
+    }
+    }
 </script>
