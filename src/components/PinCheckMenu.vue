a9a5bae2536aeba46c7092b5286fb082dee63810
hyunji-bang <groovehyunji@gmail.com>
2017-04-20 16:03:26 +0900

    모달창 스타일 변경

--- src/components/PinCheckMenu.vue
+++ src/components/PinCheckMenu.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="pin-check-menu">
-        <h2 class="pin-check-menu--heading"> 이 장소를 저장하시겠습니까? </h2>
-        <button class="pin-check-menu--button" @click="$emit('pinOK')">OK</button>
+    <div class="pin-check">
+        <strong class="pin-check__message"> 이 장소를 저장하시겠습니까? </strong>
+        <button class="pin-check-menu__button" @click="$emit('pinOK')">OK</button>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'pin-check-menu',
+    name: 'pin-check',
     data(){
         return {
         };
